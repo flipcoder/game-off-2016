c138e81225f2c3df1a9b34c67f96145d11a5cc96
Grady O'Connell <flipcoder@gmail.com>
2016-11-04 22:54:21 -0700

    intro state

---
+++ src/Intro.cpp
@@ -0,0 +1,75 @@
+#include "Intro.h"
+#include "Qor/BasicPartitioner.h"
+#include "Qor/Input.h"
+#include "Qor/Qor.h"
+#include <glm/glm.hpp>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+using namespace std;
+using namespace glm;
+
+Intro :: Intro(Qor* engine):
+    m_pQor(engine),
+    m_pInput(engine->input()),
+    m_pRoot(make_shared<Node>()),
+    m_pPipeline(engine->pipeline()),
+    m_pResources(engine->resources())
+{}
+
+void Intro :: preload()
+{
+    float sw = m_pQor->window()->size().x;
+    float sh = m_pQor->window()->size().y;
+    
+    m_pCamera = make_shared<Camera>(m_pQor->resources(), m_pQor->window());
+    m_pMusic = m_pQor->make<Sound>("music.ogg");
+    m_pRoot->add(m_pMusic);
+    m_pRoot->add(m_pCamera);
+    m_pPhysics = make_shared<Physics>(m_pRoot.get(), this);
+    //m_pPlayer = m_pQor->make<Mesh>("guy.obj");
+    //m_pRoot->add(m_pPlayer);
+
+    auto mat = make_shared<Material>("logo.png", m_pResources);
+    m_pBG = make_shared<Mesh>(
+        make_shared<MeshGeometry>(Prefab::quad(vec2(sw/2.0f, sh/2.0f), vec2(-sw/2.0f, -sh/2.0f))),
+        vector<shared_ptr<IMeshModifier>>{
+            make_shared<Wrap>(Prefab::quad_wrap(vec2(1.0f, 0.0f), vec2(0.0f, 1.0f)))
+        },
+        make_shared<MeshMaterial>(mat)
+    );
+    m_BGScale = 0.5f;
+    m_pBG->position(glm::vec3(sw/2.0f, sh/2.0f, 0.0f));
+    m_pRoot->add(m_pBG);
+}
+
+Intro :: ~Intro()
+{
+    m_pPipeline->partitioner()->clear();
+}
+
+void Intro :: enter()
+{
+    m_pCamera->ortho();
+    m_pPipeline->winding(true);
+    m_pPipeline->bg_color(Color::black());
+    m_pInput->relative_mouse(false);
+    m_pMusic->play();
+}
+
+void Intro :: logic(Freq::Time t)
+{
+    if(m_pInput->key(SDLK_ESCAPE))
+        m_pQor->quit();
+
+    m_pRoot->logic(t);
+    m_pBG->reset_orientation();
+    m_BGScale = std::min(1.0f, m_BGScale + 0.001f * t);
+    m_pBG->scale(glm::vec3(m_BGScale, m_BGScale, 1.0f));
+}
+
+void Intro :: render() const
+{
+    m_pPipeline->render(m_pRoot.get(), m_pCamera.get());
+}
+
---
+++ src/Intro.h
@@ -0,0 +1,51 @@
+#ifndef _INTROSTATE_H
+#define _INTROSTATE_H
+
+#include "Qor/Node.h"
+#include "Qor/State.h"
+#include "Qor/Input.h"
+#include "Qor/Camera.h"
+#include "Qor/Pipeline.h"
+#include "Qor/Mesh.h"
+#include "Qor/Sound.h"
+#include "Qor/Material.h"
+
+class Qor;
+
+class Intro:
+    public State
+{
+    public:
+        
+        Intro(Qor* engine);
+        virtual ~Intro();
+
+        virtual void preload() override;
+        virtual void enter() override;
+        virtual void logic(Freq::Time t) override;
+        virtual void render() const override;
+        virtual bool needs_load() const override {
+            return true;
+        }
+
+    private:
+        
+        Qor* m_pQor = nullptr;
+        Input* m_pInput = nullptr;
+        Pipeline* m_pPipeline = nullptr;
+        Cache<Resource, std::string>* m_pResources = nullptr;
+
+        std::shared_ptr<Node> m_pRoot;
+        std::shared_ptr<Camera> m_pCamera;
+        std::shared_ptr<Sound> m_pMusic;
+        std::shared_ptr<Physics> m_pPhysics;
+
+        std::shared_ptr<Mesh> m_pPlayer;
+
+        float m_BGScale = 1.0f;
+        std::shared_ptr<Mesh> m_pBG;
+};
+
+#endif
+
+
